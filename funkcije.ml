25065e407cdd3aa8f8b5c4f6df0388823d3353a8
tjasavrhovnik <tjasa.vrhovnik@student.fmf.uni-lj.si>
2017-12-17 14:53:54 +0100

    Dokoncala funkcije

--- funkcije.ml
+++ funkcije.ml
@@ -20,8 +20,7 @@ let reverse l =
  ---------- *)
 
 let rec repeat x n = 
-  if n<= 0 then [] else x :: (repeat x (n-1))
-  
+  if n <= 0 then [] else x :: (repeat x (n-1))
 
 (* Funkcija "range n" sprejme številio n in vrne seznam vseh celih števil od 0
  do vključno n. Za neprimerne n funkcija vrne prazen seznam.
@@ -40,6 +39,19 @@ let range n =
 		| n -> aux_range (n-1) (n::acc)
 	in
     aux_range n []
+	
+let range2 n =
+  let rec range2_aux n acc =
+    if n < 0 then [] else range2_aux (n-1) (n::acc)
+  in
+  range2_aux n []
+  
+let range_not_tailrec n =
+  let rec range_from m =
+    if m > n then []
+	else m :: (range_from (m+1))
+  in
+  range_from 0
 
 (* Funkcija "map f l" sprejme seznam l = [l0; l1; l2; ...] in funkcijo f
  in vrne seznam [f(l0); f(l1); f(l2); ...].
@@ -64,10 +76,18 @@ let rec map f l =
 let map_tlrec f l = 
   let rec map_aux f l acc =
     match l with
-	  | [] -> reverse acc
-	  | hd::tl -> map_aux f tl ((f hd) :: acc)
+	| [] -> reverse acc
+	| hd::tl -> map_aux f tl ((f hd) :: acc)
   in
   map_aux f l []
+  
+let map_tlrec2 f l =
+  let rec map_aux2 l res_rev =
+    match l with
+	| [] -> reverse res_rev
+	| x::xs -> map_aux2 xs (f x :: res_rev)
+  in
+  map_aux2 l []
 
 (* Funkcija "mapi f l" sprejme seznam l = [l0; l1; l2; ...] in funkcijo f
  ter vrne seznam [f 0 l0; f 1 l1; f 2 l2; ...].
@@ -84,6 +104,14 @@ let mapi f l =
   in
   mapi_aux f l [] 0
   
+let mapi2 f l =
+  let rec mapi_aux l index =
+    match l with
+	| [] -> []
+	| x::xs -> (f x index) :: (mapi_aux xs (index+1))
+  in
+  mapi_aux l 0
+  
 (* Funkcija "zip l1 l2" sprejme seznama l1 = [l1_0; l1_1; l1_2; ...] in
  l2 = [l2_0; l2_1; l2_2; ...] in vrne seznam [(l1_0,l2_0); (l1_1,l2_1); ...].
  Če seznama nista enake dolžine vrne napako.
@@ -94,15 +122,12 @@ let mapi f l =
  Exception: Failure "Seznama razlicnih dolzin.".
  ---------- *)
  
-let zip l1 l2 =
-  let rec zip_aux l1 l2 acc =
-    match (l1, l2) with
-      |([],[]) -> acc
-	  |(_::_, []) -> failwith "Seznama razlicnih dolzin."
-	  |([], _::_) -> failwith "Seznama razlicnih dolzin."
-      |(hd1::tl1, hd2::tl2) -> (hd1, hd2) :: zip_aux tl1 tl2 acc 
-  in
-  zip_aux l1 l2 []	  
+let rec zip l1 l2 =
+  match (l1, l2) with
+  | ([], []) -> []
+  | ([], _) -> failwith "Seznama razlicnih dolzin."
+  | (_, []) -> failwith "Seznama razlicnih dolzin."
+  | (x::xs, y::ys) -> (x, y) :: (zip xs ys)  
 
 (* Funkcija "zip_enum_tlrec l1 l2" sprejme seznama l1 = [l1_0; l1_1; l1_2; ...] 
  in l2 = [l2_0; l2_1; l2_2; ...] in vrne [(0, l1_0, l2_0); (1, l1_1, l2_1); ...].
@@ -117,11 +142,21 @@ let zip_enum_tlrec l1 l2 =
   let rec zip_enum_tlrec_aux l1 l2 acc i =
     match (l1, l2) with
 	  |([],[]) -> acc
-	  |(_::_, []) -> failwith "Seznama razlicnih dolzin."
-	  |([], _::_) -> failwith "Seznama razlicnih dolzin."
+	  |(_, []) | ([], _) -> failwith "Seznama razlicnih dolzin."
 	  |(hd1::tl1, hd2::tl2) -> (i, hd1, hd2) :: zip_enum_tlrec_aux tl1 tl2 acc (i+1)
   in
   zip_enum_tlrec_aux l1 l2 [] 0
+  
+let zip_enum_tlrec2 l1 l2 =
+  let rec zip_enum_aux l1 l2 index acc_rev =
+    match (l1, l2) with
+	| ([], []) -> reverse acc_rev
+	| ([], _) | (_, []) -> failwith "Seznama razlicnih dolzin."
+	| (x::xs, y::ys) ->
+	  let new_head = (index, x, y) in
+	  zip_enum_aux xs ys (index+1) (new_head::acc_rev)
+	in
+	zip_enum_aux l1 l2 0 []
 
 (* Funkcija "unzip l" sprejme seznam l = [(a0, b0); (a1, b2); ...]
  in vrne dvojico seznamov ([a0; a1; ...], [b0; b1; ...]).
@@ -130,10 +165,12 @@ let zip_enum_tlrec l1 l2 =
  - : int list * string list = ([0; 1; 2], ["a"; "b"; "c"])
  ---------- *)
 
-let unzip l = 
+let rec unzip l = 
   match l with
-    |[] -> []
-	|hd::tl -> 
+  | [] -> ([], [])
+  | (x1, x2)::xs ->
+    let (l1, l2) = unzip xs in
+	(x1::l1, x2::l2)
 
 (* Funkcija "unzip_tlrec l" je tail-recursive verzija funkcije unzip.
  ----------
@@ -141,7 +178,13 @@ let unzip l =
  - : int list * string list = ([0; 1; 2], ["a"; "b"; "c"])
  ---------- *)
 
-let unzip_tlrec l = ()
+let unzip_tlrec l = 
+  let rec unzip_tlrec_aux l acc_x acc_y =
+    match l with
+	| [] -> (reverse acc_x, reverse acc_y)
+	| (x, y)::tl -> unzip_tlrec_aux tl (x::acc_x) (y::acc_y)
+  in
+  unzip_tlrec_aux l [] []
 
 (* Funkcija "fold_left_no_acc f l" sprejme seznam l = [l0; l1; l2; ...; ln] in funkcijo f,
  vrne pa f(... (f (f (f l0 l1) l2) l3) ... ln).
@@ -151,7 +194,11 @@ let unzip_tlrec l = ()
  - : string = "FIKUS"
  ---------- *)
 
-let fold_left_no_acc f l = ()
+let rec fold_left_no_acc f l = 
+  match l with
+  | _::[] | [] -> failwith "Prekratek seznam."
+  | x::y::[] -> f x y
+  | x::y::tl -> fold_left_no_acc f ((f x y) :: tl)
 
 (* Funkcija "apply_sequence f x n" vrne seznam zaporednih uporab funkcije f na x,
  [x; f x; f (f x); ...; f uporabljena n-krat na x].
@@ -163,7 +210,13 @@ let fold_left_no_acc f l = ()
  - : int list = []
  ---------- *)
 
-let apply_sequence f x n = ()
+let apply_sequence f x n = 
+  let rec apply_sequence_aux f x n acc =
+    if n < 0 
+	then reverse acc
+	else apply_sequence_aux f (f x) (n-1) (x::acc)
+  in
+  apply_sequence_aux f x n []
 
 (* Funkcija "filter f l" sprejme seznam l in vrne seznam elementov l,
  za katere funkcija f vrne true.
@@ -172,7 +225,10 @@ let apply_sequence f x n = ()
  - : int list = [4; 5]
  ---------- *)
 
-let filter f l = ()
+let rec filter f l =
+  match l with
+  | [] -> []
+  | x::xs -> if f x then x :: (filter f xs) else (filter f xs)
 
 (* Funkcija "exists f l" sprejme seznam l in vrne true če obstaja 
 element
@@ -185,7 +241,14 @@ element
  - : bool = false
  ---------- *)
 
-let exists f l = ()
+let rec exists f l = 
+  match l with
+  | [] -> false
+  | x::xs -> if f x then true else exists f xs
+  
+let rec exists f = function
+  | [] -> false
+  | x::xs -> f x || exists f xs
 
 (* Funkcija "first f none_value l" sprejme seznam l in vrne prvi element seznama,
  za katerega funkcija f vrne true, če takšnega elementa ni, pa vrne none_value.
@@ -197,7 +260,9 @@ let exists f l = ()
  - : int = 0
  ---------- *)
 
-let first f none_value l = ()
+let rec first f none_value = function
+  | [] -> none_value
+  | x::xs -> if f x then x else first f none_value xs
   
 (* Severnjaki napadajo Medbrezje. Kot vrhovni čarodej poznaš zaporedje urokov s katerimi
  lahko Medbrezje zaščitiš pred napadom, zaporedje urokov pa je predstavljeno v seznamu oblike
@@ -232,6 +297,32 @@ let first f none_value l = ()
   ("Mr Duck", "Protect"); ("Kylo Ren", "Banish"); ("Snoop Dogg", "Blaze")]
  ----------*)
 
-let able_protectors spells wizards = ()
+let able_protectors spells wizards = 
+  (* Unzip to sum all costs. *)
+  let (_, spell_values) = unzip spells in
+  (* Sum the costs. 0::0::spell_values added so that an empty list sums to 0. *)
+  let sequence_cost = fold_left_no_acc (+) (0::0::spell_values) in
+  (* Make filter function. *)
+  let can_cast (_, wizard_ability) = (wizard_ability >= sequence_cost) in
+  (* Get only the wizards who can cast. *)
+  let mighty_wizards = filter can_cast wizards in
+  (* Extract names. *)
+  let (mighty_wizard_names, _) = unzip_tlrec mighty_wizards in
+  mighty_wizard_names
 
-let fails_on spells wizards = ()
+let fails_on spells wizards = 
+  (* Write auxiliary function that determines the first uncastable spell. *)
+  let rec gets_stuck spells wizard_ability =
+    match spells with
+	| [] -> ""
+	| (spell_name, spell_value)::tl ->
+	  if wizard_ability >= spell_value
+	  then gets_stuck tl (wizard_ability - spell_value)
+	  else spell_name
+  in
+  (* Unzip to get a list of wizzard ability values. *)
+  let (wizard_names, wizard_abilities) = unzip wizards in
+  (* Create a list of first uncastable spells. *)
+  let stuck_spells = map (gets_stuck spells wizard_ability) wizard_abilities in
+  (* Zip it back together. *)
+  zip wizard_names stuck_spells
